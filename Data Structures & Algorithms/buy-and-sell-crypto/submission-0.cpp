0d2cd7457b4a77931600563b01f406cf88ca55f7
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-06-26 14:31:04 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit=0;
+        int min_price=INT_MAX;
+        for(int price:prices){
+            if(price<min_price) min_price=price;
+            else max_profit=max(max_profit,price-min_price);
+        }return max_profit;
+    }
+};
